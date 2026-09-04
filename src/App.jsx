45bc29e7ae065f8ea977f7d1e367e72cb2ebaad7
yarimatechs <yareemaabdul10@gmail.com>
2026-09-04 22:41:50 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1273,8 +1273,9 @@ IMPORTANT RULES:
                   style={{ height: "100%", width: "100%", minHeight: 320 }}
                 >
                   <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                    subdomains="abcd"
                   />
                   <RecenterMap lat={location.lat} lon={location.lon} />
                   <Marker position={[location.lat, location.lon]} icon={userIcon}>
